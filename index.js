48f7d1951dde1489ddedf12e7d76ba091cb92c99
Angel <angmejia0324@gmail.com>
2023-04-08 19:40:36 -0600

    fix al cors

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import "./database/connectdb.js";
 import express from "express";
 import authRouter from "./routes/auth.route.js";
 import linkRouter from "./routes/link.route.js";
-import taskRouter from "./routes/task.route.js"
+import taskRouter from "./routes/task.route.js";
 import cookieParser from "cookie-parser";
 import redirectRouter from "./routes/redirect.route.js";
 import cors from "cors";
@@ -14,11 +14,12 @@ const whiteList = [process.env.ORIGIN1];
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (whiteList.includes(origin) || "undefined") {
+      if (!origin || whiteList.includes(origin)) {
         return callback(null, origin);
       }
       return callback("Error de CORS origin: " + origin + " No autorizado!");
     },
+    credentials: true,
   })
 );
 
